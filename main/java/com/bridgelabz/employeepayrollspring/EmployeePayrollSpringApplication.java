95a71726bd84a9ef9d8a062e461d3273f77b50b9
viresh241196 <vireshrawool007@gmail.com>
2021-04-28 19:13:48 +0530

    using @SIF4j for logging in file

--- main/java/com/bridgelabz/employeepayrollspring/EmployeePayrollSpringApplication.java
+++ main/java/com/bridgelabz/employeepayrollspring/EmployeePayrollSpringApplication.java
@@ -1,13 +1,16 @@
 package com.bridgelabz.employeepayrollspring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+        log.info("Employee payroll app started");
     }
-
 }
--- main/java/com/bridgelabz/employeepayrollspring/exception/EmployeeException.java
+++ main/java/com/bridgelabz/employeepayrollspring/exception/EmployeeException.java
@@ -1,14 +1,56 @@
 package com.bridgelabz.employeepayrollspring.exception;
 
+import com.bridgelabz.employeepayrollspring.DTO.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
 public class EmployeeException extends RuntimeException{
-    private final exceptionType type;
 
-    public EmployeeException(exceptionType type,String message) {
-        super(message);
-        this.type = type;
-    }
-
-    public enum exceptionType {
-        EMPLOYEE_NOT_FOUND,
-    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception){
+        List<ObjectError> errorList=exception.getBindingResult().getAllErrors();
+        List<String> errMsg =errorList.stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+        ResponseDTO responseDTO =
+                new ResponseDTO("Exception while processing the Rest request",errMsg);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+        }
 }
+
+//@ControllerAdvice
+//public class EmployeeControllerValidation extends ResponseEntityExceptionHandler {
+//
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+//                                                                  HttpHeaders headers,
+//                                                                  HttpStatus status,
+//                                                                  WebRequest request) {
+//        String errorMsg = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+//        return new ResponseEntity<>(
+//                new ResponseDTO(null, errorMsg),
+//                HttpStatus.BAD_REQUEST);
+//    }
+//}
+
+
+
+//    private final exceptionType type;
+//
+//    public EmployeeException(exceptionType type,String message) {
+//        super(message);
+//        this.type = type;
+//    }
+//
+//public enum exceptionType {
+//    EMPLOYEE_NOT_FOUND,
+//}
